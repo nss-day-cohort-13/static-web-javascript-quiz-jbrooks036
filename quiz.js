eb8b32b7e385b8884a633beb6cedd9212a3bb480
jbrooks036 <joannbrooks036@gmail.com>
2016-04-20 10:22:59 -0500

    full tree - yay

--- quiz.js
+++ quiz.js
@@ -1,24 +1,26 @@
 var btn = document.getElementById("grow-button");
 
 btn.addEventListener("click", function(){
-    alert("button-clicked");
     paintTree();
 });
 
 function paintTree () {
-	var treeHeight = document.getElementById("tree-height").value;
+	var treeHeight = parseInt(document.getElementById("tree-height").value);
   var leafChar = document.getElementById("leaf-char").value;
 
-  for (var i = 1; i <= treeHeight + 1; i++)
+  for (var i = 1; i < treeHeight + 1; i++)
   {
     var tier = "";
-    for (var j = treeHeight; j > 0 ; j--) {
+    for (var j = treeHeight - i; j > 0 ; j--) {
       tier += " ";
     } 
-    for (var k = 1; k < i; k++) {
+    
+    for (var k = 0; k < (2*i) - 1; k++) {
       tier += leafChar;
     } 
+   
     console.log(tier);
+   
   }
 }
 
